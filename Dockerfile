e6dfeaca9f2dba0552b493abe4ef2ad204caa059
Ashraf almouaie <ashrafalmouaie>
2026-06-12 03:35:02 -0500

    prepare for deployment

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ RUN pip install -r requirements.txt
 
 COPY . . 
 
-CMD ["python","manage.py","runserver","0.0.0.0:8000"]
+EXPOSE 8000
+
+CMD ["sh", "-c", "python manage.py migrate && gunicorn mysite.wsgi:application --bind 0.0.0.0:8000"]
--- mysite/settings.py
+++ mysite/settings.py
@@ -23,12 +23,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7*574&tnic%b-f!tpecnh#g=ji8nvx!4h*!%h=i@9$v!0wegh='
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =config("DEBUG",default=False,cost=bool)
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="localhost,127.0.0.1"
+).split(",")
+
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=config("DATABASE_URL")
+    )
+}
 
 
 # Application definition
@@ -119,12 +130,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
